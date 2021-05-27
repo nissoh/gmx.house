111e07b3885e4ac41a7001722c6af864878443b1
Nissan Hanina <nissanhanina@gmail.com>
2021-05-27 18:25:53 +0300

    init backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,34 +1,19 @@
 import Koa from 'koa'
-import { Storage } from '@google-cloud/storage'
+import useStatic from 'koa-static'
+import path from 'path'
+
+const staticDirPath = path.join(__dirname, '../../../frontend/.dist')
+
 const app = new Koa()
 
+app.use(useStatic(staticDirPath))
+
+
 app.use(async ctx => {
-  ctx.body = 'Hello World'
+  console.log(ctx)
+  // ctx.body = file
 })
 
+console.log(`Running server http://localhost:8080`)
 
-
-// For more information on ways to initialize Storage, please see
-// https://googleapis.dev/nodejs/storage/latest/Storage.html
-
-// Creates a client using Application Default Credentials
-const storage = new Storage({
-  
-})
-
-// Creates a client from a Google service account key
-// const storage = new Storage({keyFilename: 'key.json'});
-
-
-// The ID of your GCS bucket
-const bucketName = 'your-unique-bucket-name'
-
-async function createBucket() {
-  // Creates the new bucket
-  await storage.createBucket(bucketName)
-  console.log(`Bucket ${bucketName} created.`)
-}
-
-createBucket().catch(console.error)
-
-app.listen(3000)
+app.listen(8080)
