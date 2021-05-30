016c71cce8b4059f9e833e42ad3b901c9b1cd0bf
Nissan Hanina <nissanhanina@gmail.com>
2021-05-31 01:11:06 +0300

    fix static dir

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -177,7 +177,7 @@ const run = async () => {
   
 
   // app.use(express.json())
-  app.use(express.static('../../../frontend/dist'))
+  app.use(express.static(__dirname + './../../../frontend/dist'))
   app.use((req, res, next) => RequestContext.create(ORM.em, next))
   app.use('/api', leaderboardApi)
   app.use((req, res) => res.status(404).json({ message: 'No route found' }))
