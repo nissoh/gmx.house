7df237500469b4b5addeedaf32d4b503c9b1a021
Nissan Hanina <nissanhanina@gmail.com>
2021-08-18 22:33:19 +0300

    fix backend build

--- backend/src/logic/screenshot-og.ts
+++ backend/src/logic/screenshot-og.ts
@@ -1,10 +1,5 @@
 import puppeteer from 'puppeteer-core'
 
-// helper function just in case to give a page some time to render things after loading
-
-
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
-
 
 const exePath =
   process.platform === "win32"
@@ -13,22 +8,7 @@ const exePath =
       ? "/usr/bin/google-chrome"
       : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 
-async function runPuppeteer(isDev: boolean) {
-  // let options
-  // if (isDev) {
-  //   options = {
-  //     args: [],
-  //     executablePath: exePath,
-  //     headless: true,
-  //   }
-  // } else {
-  //   options = {
-  //     args: chrome.args,
-  //     executablePath: await chrome.executablePath,
-  //     headless: chrome.headless,
-  //   }
-  // }
-
+async function runPuppeteer() {
   const browser = await puppeteer.launch({
     args: [],
     executablePath: exePath,
@@ -43,11 +23,10 @@ async function runPuppeteer(isDev: boolean) {
 
   return { browser, page }
 }
-// const puppeteerInstance = runPuppeteer(true)
 
 export async function screenPage(url: string) {
 
-  const instance = await runPuppeteer(true)
+  const instance = await runPuppeteer()
 
   await instance.page.goto(url, { waitUntil: 'networkidle0' })
 
