4f72dffe69f41648d3745dafca588f8e8b21b6d6
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2022-11-16 21:33:31 +0200

    fix priority

--- backend/src/logic/competition-cumulative-pnl.ts
+++ backend/src/logic/competition-cumulative-pnl.ts
@@ -12,6 +12,7 @@ import { fetchHistoricTrades, graphMap } from "./api"
 import { gql, TypedDocumentNode } from "@urql/core"
 
 
+const bigNumberForPriority = 10n ** 50n
 
 const createCache = cacheMap({})
 
@@ -60,8 +61,8 @@ export const competitionCumulativePnl = O(
       const claimMap = groupByMap(claimList, item => item.account.toLowerCase())
       const formattedList = toAccountCompetitionSummary(tradeList, priceMap)
         .sort((a, b) => {
-          const aN = claimMap[a.account] ? a.realisedPnl + a.openPnl : a.realisedPnl + a.openPnl - 100000000n
-          const bN = claimMap[b.account] ? b.realisedPnl + b.openPnl : b.realisedPnl + b.openPnl - 100000000n
+          const aN = claimMap[a.account] ? a.realisedPnl + a.openPnl : a.realisedPnl + a.openPnl - bigNumberForPriority
+          const bN = claimMap[b.account] ? b.realisedPnl + b.openPnl : b.realisedPnl + b.openPnl - bigNumberForPriority
 
           return Number(bN - aN)
         })
